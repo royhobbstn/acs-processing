b02ce3061180c6cb0e63862ff6be1cb6eadfc542
Daniel Trone <royhobbstn@yahoo.com>
2015-12-07 15:32:50 +0000

    add streamline and cleanup for database and filesystem cleanup

---
+++ cleanup.js
@@ -0,0 +1,15 @@
+var rmdir = require( 'rmdir' );
+var path  = 'temp';
+var path2  = 'scan';
+
+rmdir( path, function ( err, dirs, files ){
+  console.log( dirs );
+  console.log( files );
+  console.log( 'all temp directory files are removed' );
+});
+
+rmdir( path2, function ( err, dirs, files ){
+  console.log( dirs );
+  console.log( files );
+  console.log( 'all scan directory files are removed' );
+});
